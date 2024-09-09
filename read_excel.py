d43a56591d941505faffdf36caf2f66dc25413eb
Andrew Gibbons <gibbonsand@gmail.com>
2024-09-09 23:54:58 +0200

    feat: read excel

--- read_excel.py
+++ read_excel.py
@@ -1,2 +1,13 @@
-import openpyxl
+from openpyxl import load_workbook
+import pandas as pd
 
+wb = pd.read_excel(
+    '/Users/andrewgibbons/Downloads/Book 5.xlsx',
+    sheet_name = 'YGO',
+    header=0,
+    #names=['Name', 'Amoount', 'In decks', 'SD', 'SD in decks', 'In Draft', 'In Bulk', 'SD in bulk']
+)
+
+
+print(wb)
+print(wb.columns)
