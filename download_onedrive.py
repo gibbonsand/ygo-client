45e55de5c61b1697d103b81abce0ced761247e92
Andrew Gibbons <gibbonsand@gmail.com>
2024-09-09 23:42:57 +0200

    feat: restructure files

--- download_onedrive.py
+++ download_onedrive.py
@@ -1,18 +1,15 @@
-import json
-import requests
-
 from onedrive_api.onedrive_api_client import OneDriveAPIClient
-import onedrive_api.constants as c
 
 
-if __name__ == '__main__':
-    
-    # DOwnload file from OneDrive
+def download_file(filename: str = "Book 5.xlsx") -> None:
+    # Download file from OneDrive
     client = OneDriveAPIClient()
     client.authenticate()
 
     filename = 'Book 5.xlsx'
     file_id = client.lookup_file(filename)
-    response = client.download_file(
+    
+    # Download file
+    client.download_file(
         file_id,
         f"/Users/andrewgibbons/Downloads/{filename}")
---
+++ main.py
@@ -0,0 +1,4 @@
+from download_onedrive import download_file
+
+if __name__ == "__main__":
+    download_file()
